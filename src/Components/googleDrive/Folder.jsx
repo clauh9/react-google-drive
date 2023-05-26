c01d10b405830c8c0f883bf21d87f571e3a8e928
Claudia Miranda <clauh9@outlook.pt>
2023-05-26 16:31:38 +0100

    FIX ME: DELETE

--- src/Components/googleDrive/Folder.jsx
+++ src/Components/googleDrive/Folder.jsx
@@ -1,31 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { database } from "../../firebase";
-import {
-	Button,
-	Dropdown,
-	OverlayTrigger,
-	Popover,
-	Toast,
-} from "react-bootstrap";
+import { Button, OverlayTrigger, Popover } from "react-bootstrap";
 import { Folder as F, ThreeDotsVertical } from "react-bootstrap-icons";
+import deleteFolders from "../utils/deleteFolders";
 
 const Folder = ({ folder }) => {
-	const [showB, setShowB] = useState(false);
-	const toggleShowB = () => setShowB(showB);
-	const threeDots = (e) => {
+	// Deletes the folder from Firebase
+
+	const deleteFolder = (e) => {
 		e.preventDefault();
-		setShowB(!showB);
+		const parentId = folder.parentId; // Get the parent folder's ID
+
+		// deleteChildFolders(folder.id); // Delete child folders recursively
+
+		database.folders.doc(folder.id).delete(); // Delete the current folder
 	};
 
-	function deleteFolder(e) {
-		e.preventDefault();
-		console.log(folder.id);
-		database.folders.doc(folder.id).delete();
-	}
+	const deleteChildFolders = (parentId) => {
+		database.folders
+			.where("parentId", "==", parentId)
+			.get()
+			.then((querySnapshot) => {
+				querySnapshot.forEach((doc) => {
+					const folderId = doc.id;
+					deleteChildFolders(folderId); // Recursively delete child folders
+					database.folders.doc(folderId).delete(); // Delete the child folder
+				});
+			})
+			.catch((error) => {
+				console.error("Error deleting child folders:", error);
+			});
+	};
 
+	// The popup that shows up when we click the 3 dots
 	const popover = (
-		<Popover id="popover-basic">
+		<Popover>
 			<Popover.Body onClick={deleteFolder}>Delete Folder</Popover.Body>
 		</Popover>
 	);
@@ -41,20 +51,16 @@ const Folder = ({ folder }) => {
 				className="text-truncate w-100"
 				as={Link}
 			>
-				{/* <F /> {folder.name} <ThreeDotsVertical onClick={threeDots} /> */}
 				<F /> {folder.name}{" "}
 				<OverlayTrigger
 					trigger="click"
 					placement="bottom"
 					overlay={popover}
-					delay={{ show: 250, hide: 400 }}
+					rootClose //so it closes when you click somewhere else
 				>
 					<ThreeDotsVertical onClick={(e) => e.preventDefault()} />
 				</OverlayTrigger>
 			</Button>
-			{/* <Toast onClose={toggleShowB} show={showB} animation={false}>
-				<Toast.Body>Delete</Toast.Body>
-			</Toast> */}
 		</>
 	);
 };
